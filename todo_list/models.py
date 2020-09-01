06d41afef0f38d60862fd77b1cb51ef4d1a1bbec
quratulain25 <quratulain@arbisoft.com>
2020-09-01 12:40:51 +0500

    models created and gitignore modeified.

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=10)
+
+
+class Task(models.Model):
+    page_id = models.ForeignKey(Page, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    is_complete = models.BooleanField(default=False)
