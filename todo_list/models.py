504529f2bfc7d1af93817835b5c305d4bb65dd44
quratulain25 <quratulain@arbisoft.com>
2020-09-04 16:52:04 +0500

    requested changes made.

--- todo_list/models.py
+++ todo_list/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Page(models.Model):
+    title = models.CharField(max_length=32)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True, editable=True)
+
     def __str__(self):
         return self.title
 
-    title = models.CharField(max_length=30)
-    create_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True, editable=True)
-
 
 class Task(models.Model):
-    def __str__(self):
-        return self.page_id.title
-
     page_id = models.ForeignKey(Page, on_delete=models.CASCADE)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=256)
     is_complete = models.BooleanField(default=False)
     create_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True, editable=True)
+
+    def __str__(self):
+        return self.page_id.title
