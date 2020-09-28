08327b806dbb7f9968f0113fa84ea7508b97575d
quratulain25 <quratulain@arbisoft.com>
2020-09-28 14:09:43 +0500

    formatting.

--- serendipity/urls.py
+++ serendipity/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 
 from serendipity import settings
 
-urlpatterns = [path('admin/', admin.site.urls),
-               path('accounts/', include('django.contrib.auth.urls')),
-               path('', include('todo_list.urls'))] + static(settings.STATIC_URL)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('todo_list.urls')),
+] + static(settings.STATIC_URL)
